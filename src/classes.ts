f83a6cd506996247d1625b528fc88ebbe745624d
Chris Koorzen <fckoorzen@gmail.com>
2024-11-19 15:30:53 +0200

    Create 'MainProperty' class

---
+++ src/classes.ts
@@ -0,0 +1,18 @@
+import { Review } from "./interfaces.js"
+
+
+export class MainProperty {
+    src: string
+    title: string
+    reviews: Review[]
+
+    constructor(
+        src: string,
+        title:string,
+        reviews: Review[]
+    ) {
+        this.src = src
+        this.title = title
+        this.reviews = reviews
+    }
+}
