d3bf0918f690a18959b0f9fa8ccc4f5d0c1f8f45
Chris Koorzen <fckoorzen@gmail.com>
2024-11-19 15:05:21 +0200

    Add comments, format function layouts to be the same

--- src/utils.ts
+++ src/utils.ts
@@ -9,28 +9,46 @@ const userNameDisplay = document.querySelector('#user')
 
 
 // Functionality
-export function showReviewTotal (value : number, reviewer: string, isLoyalty : LoyaltyUser): void {
-    const iconDisplay = (isLoyalty === LoyaltyUser.GOLD_USER) ? '⭐' : ''
+export function showReviewTotal(
+    value : number,
+    reviewer: string,
+    isLoyalty : LoyaltyUser
+
+) : void {
+    const iconDisplay = (isLoyalty === LoyaltyUser.GOLD_USER) ? '⭐' : ''       // Bug (fixed): if no comparison is done, it will always return true
     reviewTotalDisplay.innerHTML = `${value.toString()} review${makeMultiple(value)} | last reviewed by ${reviewer} ${iconDisplay}`
 }
 
-function makeMultiple(value: number) : string {
+
+function makeMultiple(
+    value: number
+
+) : string {
     if (value > 1) {
         return 's'
     } else return ''
 }
 
 
-export function populateUser(isReturning: boolean, userName: string ): void {
+export function populateUser(
+    isReturning: boolean,
+    userName: string 
+
+) : void {
     if (isReturning){
-        returningUserDisplay.innerHTML = 'back'
+        returningUserDisplay.innerHTML = 'back'     // Bug: This value will be overridden by the next line
     }
     userNameDisplay.innerHTML = userName
 }
 
 
-export function showDetails(authorityStatus: Permission, element : HTMLElement, price: number): void {
-    if ( authorityStatus === Permission.ADMIN ){
+export function showDetails(        
+    authorityStatus: Permission,
+    element : HTMLElement,
+    price: number
+
+) : void {
+    if ( authorityStatus === Permission.ADMIN ){    // Bug (fixed): without strict checking any truthy value will grant access
         const priceDisplay = document.createElement('div')
         priceDisplay.innerHTML = price.toString() + '/night'
         element.appendChild(priceDisplay)
