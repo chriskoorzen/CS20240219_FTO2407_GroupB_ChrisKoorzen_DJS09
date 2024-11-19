13adbfe4e062d18de071582404557b070ef42eee
Chris Koorzen <fckoorzen@gmail.com>
2024-11-19 10:37:56 +0200

    Add boolean variable 'isOpen'

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 
 
+// Variables
+let isOpen: boolean
+
 
 // Functionality
 function showReviewTotal (value : number, reviewer: string, isLoyalty : boolean) {
