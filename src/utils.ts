435fd6d2b6871fc08c2da00588ca21a94859d469
Chris Koorzen <fckoorzen@gmail.com>
2024-11-19 14:29:06 +0200

    Add review multiple function, add return typedefs to functions

--- src/utils.ts
+++ src/utils.ts
@@ -8,13 +8,19 @@ const userNameDisplay = document.querySelector('#user')
 
 
 // Functionality
-export function showReviewTotal (value : number, reviewer: string, isLoyalty : LoyaltyUser) {
+export function showReviewTotal (value : number, reviewer: string, isLoyalty : LoyaltyUser): void {
     const iconDisplay = (isLoyalty === LoyaltyUser.GOLD_USER) ? '⭐' : ''
-    reviewTotalDisplay.innerHTML = `review total ${value.toString()} | last reviewed by ${reviewer} ${iconDisplay}`
+    reviewTotalDisplay.innerHTML = `${value.toString()} review${makeMultiple(value)} | last reviewed by ${reviewer} ${iconDisplay}`
+}
+
+function makeMultiple(value: number) : string {
+    if (value > 1) {
+        return 's'
+    } else return ''
 }
 
 
-export function populateUser(isReturning: boolean, userName: string ) {
+export function populateUser(isReturning: boolean, userName: string ): void {
     if (isReturning){
         returningUserDisplay.innerHTML = 'back'
     }
@@ -22,7 +28,7 @@ export function populateUser(isReturning: boolean, userName: string ) {
 }
 
 
-export function showDetails(authorityStatus: Permission, element : HTMLElement, price: number) {
+export function showDetails(authorityStatus: Permission, element : HTMLElement, price: number): void {
     if ( authorityStatus === Permission.ADMIN ){
         const priceDisplay = document.createElement('div')
         priceDisplay.innerHTML = price.toString() + '/night'
