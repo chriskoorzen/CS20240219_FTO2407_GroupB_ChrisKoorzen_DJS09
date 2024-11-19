1c77e032ed51a8d74af550cdfea768f1ad69520d
Chris Koorzen <fckoorzen@gmail.com>
2024-11-19 10:58:11 +0200

    Add method to display properties

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 const reviewTotalDisplay = document.querySelector('#reviews')
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
+const propertyContainer = document.querySelector('.properties')
 
 
 // Variables
@@ -137,3 +138,14 @@ const properties: {
 // Run
 showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 populateUser(you.isReturning, you.firstName)
+
+//Add the properties
+for (let i = 0; i < properties.length; i++) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML = properties[i].title
+    const image = document.createElement('img')
+    image.setAttribute('src', properties[i].image)
+    card.appendChild(image)
+    propertyContainer.appendChild(card)
+}
